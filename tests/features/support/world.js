3bd919464fe9c1398ecac79a488b45457b482d33
deesam <sameer.r.deshpande@tieto.com>
2017-04-13 00:25:22 +0530

    Adding headless testing

--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -1,11 +1,13 @@
-require('chromedriver');
+
 var seleniumWebdriver = require('selenium-webdriver');
+var phantomjs = require('phantomjs');
 var {defineSupportCode} = require('cucumber');
 
 function CustomWorld() {
     this.driver = new seleniumWebdriver.Builder()
-        .forBrowser('chrome')
-        .build();
+        .withCapabilities({"phantomjs.binary.path":phantomjs.path})
+  		.forBrowser('phantomjs')
+  		.build();
 }
 
 defineSupportCode(function({setWorldConstructor}) {
